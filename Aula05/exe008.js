93dfd91fe217782ef4789704597bfcc14a3573d7
Artur Morais Brasileiro <devartur.01@gmail.com>
2023-02-12 17:41:05 -0300

    Avanço no estudo de JS

---
+++ Aula05/exe008.js
@@ -0,0 +1,8 @@
+var vel = 81
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 80){
+    console.log('Você passou a velocidade permitida. MULTADO!')
+} 
+console.log('Dirija sempre usando cinto de segurança!')
+
+
