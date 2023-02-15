95053e08ac11abd867422ddbcafeba9e7c5b900b
Artur Morais Brasileiro <devartur.01@gmail.com>
2023-02-15 15:18:47 -0300

    Avanço nos estudos de JS

---
+++ Aula09/ambiente.js
@@ -0,0 +1,8 @@
+let a = [1, 3, 5]
+a.push(11, 4)
+
+console.log(a)
+console.log(`O vetor tem ${a.length} posições.`)
+console.log(`O primeiro valor do vetor é ${a[0]}.`)
+let pos = a.indexOf(3)
+console.log(`O valor 3 se encontra na posição ${pos}.`)
