25d05da685afbdec61f1c709bbe0e119e0037867
Artur Morais Brasileiro <arturbrasileiro00@gmail.com>
2025-03-10 11:06:51 -0300

    Básico JavaScript
    O básico do básico em JS.

---
+++ Variaveis e Operadores/aula.js
@@ -0,0 +1,36 @@
+// Noções básicas JS.
+
+// VARIÁVEIS.
+// const não pode ser alterado.
+const nome = "Artur";
+// let pode ser alterado.
+let idade = 20;
+
+// OPERADORES.
+let num1 = 10;
+let num2 = 5;
+
+// Soma.
+let soma = num1 + num2;
+// Subtração.
+let sub = num1 - num2;
+// Multiplicação.
+let mult = num1 * num2;
+// Divisão.
+let div = num1 / num2;
+// Resto.
+let resto = num1 % num2;
+
+// CONDICIONAIS.
+
+if (num1 > num2) {
+  console.log("num1 é maior que num2.");
+}
+ 
+else if (num1 < num2) {
+  console.log("num1 é menor que num2.");
+} 
+
+else {
+  console.log("num1 é igual a num2.");
+}
